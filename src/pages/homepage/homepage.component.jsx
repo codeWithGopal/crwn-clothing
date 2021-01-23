1875298fe0f2c233f96e3b6cea0f72d67c3ebc89
Gopal Kotwal <gopalkotwal@outlook.com>
2021-01-22 22:41:59 -0500

    broken our homepage into smaller components, finished stlying our menu-item component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Directory from "../../components/directory/directory.component";
+
+import "./homepage.styles.scss"; // importing the new saas file
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
